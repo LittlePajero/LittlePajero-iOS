d4ad2500db48f71c8790ddd7f1d56a84da78f2fe
ivyxuan <iloveivyxuan@gmail.com>
2017-05-05 16:31:23 +0800

    start the project

---
+++ LittlePajero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LittlePajero
+//
+//  Created by ivyxuan on 2017/5/5.
+//  Copyright © 2017年 ivyxuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
