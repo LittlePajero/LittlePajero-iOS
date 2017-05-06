5a4ae7dceeb8bbfcd2137689e0b265177823fa96
ivyxuan <iloveivyxuan@gmail.com>
2017-05-06 11:54:52 +0800

    add MapBox and some buttons

--- LittlePajero/ViewController.swift
+++ LittlePajero/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import Mapbox      // 引入超级好用的地图
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MGLMapView!
+    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var subButtonOne: UIButton!
+    @IBOutlet weak var subButtonTwo: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // 设置地图中心为用户坐标
+        mapView.userTrackingMode = .follow
+        
+        // 设置 MainButton 的样式
+        setMainButtonStyle()
+        
+        // 设置 SubButtons 的样式
+        setSubButtonStyle()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // 设置 MainButton 的 style
+    func setMainButtonStyle() {
+        mainButton.layer.cornerRadius = mainButton.bounds.size.height / 2
+        mainButton.layer.borderWidth = 3.0
+        mainButton.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    // 设置 SubButton 的样式
+    func setSubButtonStyle() {
+        subButtonOne.layer.backgroundColor = UIColor.gray.cgColor
+        subButtonOne.layer.cornerRadius = subButtonOne.bounds.size.height / 2
+        subButtonOne.setTitleColor(.white, for: .normal)
+    
+        subButtonTwo.layer.backgroundColor = UIColor.gray.cgColor
+        subButtonTwo.layer.cornerRadius = subButtonOne.bounds.size.height / 2
+        subButtonTwo.setTitleColor(.white, for: .normal)
+    }
 
 }
 
